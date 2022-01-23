0133100011c3c5ade6c44e9f75791907279e19d1
pickles-pickles <75488028+pickles-pickles@users.noreply.github.com>
2022-01-24 00:51:35 +0200

    Recipe create UI: updated, DB: updated: ,, UI pages: added (food, add food) ,, TO-DO: category??

--- workingdir/app.py
+++ workingdir/app.py
@@ -21,10 +21,54 @@ def index():
         newRecipe.createRecipe()
         #createRecipe(name, category)
     
-    recipes = newRecipe.selectRecipes()
-    users = selectUsers()
+    recipes = Recipe.selectRecipes()
+    """ users = selectUsers() """
 
-    return render_template('index.html', recipes = recipes, users = users)
+    return render_template('index.html', recipes = recipes, )
 
+""" if __name__ == "__main__":
+    app.run(debug=True) """
+
+@app.route('/food.html')
+def food():
+    foods = Recipe.selectRecipes()
+    return render_template('food.html', foods = foods,)  
+
+@app.route('/category.html')
+def category():
+    
+    return render_template('category.html', )
+
+@app.route('/user.html')
+def user():
+    
+    return render_template('user.html', )
+
+
+
+@app.route('/add-recipe.html', methods=['GET', 'POST'])
+def add_recipe():
+    if request.method == "GET":
+        pass
+
+    if request.method == 'POST':
+        name = request.form.get('name')
+        description = request.form.get('description')
+        img_url = request.form.get('img_url')
+        difficulty = request.form.get('difficulty')
+        category = request.form.get('category')
+        global newRecipe
+        newRecipe = Recipe(name, description, img_url, difficulty, category)
+        newRecipe.createRecipe()
+        #createRecipe(name, category)
+    
+    recipes = Recipe.selectRecipes()
+    
+
+    return render_template('/food/add-recipe.html', recipes = recipes, )
+
+""" 
+Always keep this if statement in the bottom
+ """
 if __name__ == "__main__":
     app.run(debug=True)
--- workingdir/models.py
+++ workingdir/models.py
@@ -61,32 +61,38 @@ deleteRecipe("chocolate cake")
 """ OBJECT ORIENTD PROGRAMMING STUFF """
 
 class Recipe:
-    def __init__(self, name, category, difficulty):
+    def __init__(self, name, description, img_url, difficulty, category):
         self.name = name
-        self.category = category
+        self.description = description
+        self.img_url = img_url
         self.difficulty = difficulty
+        self.category = category
+        
 
-    
+    @classmethod
     def selectRecipes(self):
         mycursor.execute("SELECT * FROM test.recipes")
         myresult = mycursor.fetchall()
         return myresult
     
     def createRecipe(self):
-        sql = "INSERT INTO test.recipes (recipename, recipecategory) VALUES (%s, %s)"
-        mycursor.execute(sql, (self.name, self.category))
+        sql = "INSERT INTO test.recipes (recipename, recipe_description, recipe_img, recipedifficulty, recipecategory) VALUES (%s, %s, %s, %s, %s)"
+        mycursor.execute(sql, (self.name, self.description, self.img_url, self.difficulty, self.category))
         mydb.commit()
         print(mycursor.rowcount, "record inserted.")
 
     def updateRecipe(self):
         sql_update_query = """UPDATE test.recipes SET recipecategory = %s WHERE recipename = %s"""
-        input_data = (self.category,self.name )
+        input_data = (self.name, self.description, self.img_url, self.difficulty, self.category )
         mycursor.execute(sql_update_query, input_data)
         mydb.commit()
         print(mycursor.rowcount,"record updated successfully ")
 
     def deleteRecipe(self):
         sql = """ DELETE FROM test.recipes  WHERE recipename = %s """ 
-        mycursor.execute(sql, (self.name,))
+        mycursor.execute(sql, (self.name, self.description, self.img_url, self.difficulty, self.category))
         mydb.commit()
         print(mycursor.rowcount, "record deleted.")
+
+
+        
